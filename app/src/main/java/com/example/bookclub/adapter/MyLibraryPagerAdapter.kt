58309fe0745041c5721402aca2cefff67716d94d
EunhaKyeong <kyeongeh424@gmail.com>
2021-08-19 01:21:03 +0900

    Feat: 추가 서브필터 관련 함수 추가
    - frameLayout에 모든 뷰 제거하는 함수 추가
    - frameLayout visibility 속성 변경하는 함수 추가
    - 각 동그라미 필터에 맞춰 frameLayout의 fragment 전환하는 함수 추가

--- app/src/main/java/com/example/bookclub/adapter/MyLibraryPagerAdapter.kt
+++ app/src/main/java/com/example/bookclub/adapter/MyLibraryPagerAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.bookclub.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bookclub.R
 import com.example.bookclub.databinding.BookListPagerBinding
+import com.example.bookclub.fragment.SortFilterFragment
 
 class MyLibraryPagerAdapter() : RecyclerView.Adapter<MyLibraryPagerAdapter.MyLibraryPagerViewHolder>() {
     private lateinit var binding: BookListPagerBinding
@@ -32,7 +36,30 @@ class MyLibraryPagerAdapter() : RecyclerView.Adapter<MyLibraryPagerAdapter.MyLib
     }
 
     class MyLibraryPagerViewHolder(itemView: BookListPagerBinding) : RecyclerView.ViewHolder(itemView.root) {
-        val filterLayout: LinearLayout = itemView.filterLayout
+        val filterLayout: FrameLayout = itemView.filterLayout
         val recyclerView: RecyclerView = itemView.bookListRecyclerView
+    }
+
+    fun clearFilterLayout() {
+        binding.filterLayout.removeAllViews()
+    }
+
+    fun setVisibilityFilterLayout(visibility: Int) {
+        binding.filterLayout.visibility = visibility
+    }
+
+    fun addFilterLayout(fragmentManager: FragmentManager, radioId: Int) {
+        binding.filterLayout.removeAllViews()
+        when (radioId) {
+            R.id.searchButton -> {
+
+            }
+            R.id.clubButton -> {
+
+            }
+            R.id.sortButton -> {
+                fragmentManager.beginTransaction().replace(binding.filterLayout.id, SortFilterFragment()).commit()
+            }
+        }
     }
 }
