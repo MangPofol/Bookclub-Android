c365709a89bc78c8d0364da406797577e578b3e1
EunhaKyeong <kyeongeh424@gmail.com>
2021-08-22 00:25:15 +0900

    Feat: 추가 ViewModel 클래스 초기화

--- app/src/main/java/com/example/bookclub/fragment/MyLibraryFragment.kt
+++ app/src/main/java/com/example/bookclub/fragment/MyLibraryFragment.kt
@@ -1,25 +1,24 @@
 package com.example.bookclub.fragment
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
+import com.example.bookclub.MainActivity
 import com.example.bookclub.R
 import com.example.bookclub.adapter.MyLibraryPagerAdapter
 import com.example.bookclub.databinding.FragmentMyLibraryBinding
+import com.example.bookclub.viewmodel.MainFilterViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MyLibraryFragment : Fragment() {
     private lateinit var binding: FragmentMyLibraryBinding
-    private lateinit var myLibraryPagerAdapter: MyLibraryPagerAdapter
-
-    private var searchFlag = 0
-    private var clubFlag = 0
-    private var sortFlag = 0
+    private lateinit var mainFilterViewModel: MainFilterViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,30 +28,24 @@ class MyLibraryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMyLibraryBinding.inflate(inflater, container, false)
+        binding = FragmentMyLibraryBinding.inflate(inflater, container, false)  //뷰바인딩 초기화
+        mainFilterViewModel = ViewModelProvider(activity as MainActivity)[MainFilterViewModel::class.java]  //메인 필터 뷰모델 초기화
 
-        //viewPager adapter 설정
-        myLibraryPagerAdapter = MyLibraryPagerAdapter()
-        binding.viewPager.adapter = myLibraryPagerAdapter
+        binding.viewPager.adapter = MyLibraryPagerAdapter(context as FragmentActivity)  //어댑터 설정
 
-        //tablayout이랑 viewPager 연결
-        TabLayoutMediator(binding.readTypeTabLayout, binding.viewPager) { tab, position ->
-            Log.d("position", position.toString())
-            when (position) {
-                0 -> tab.text = getString(R.string.reading)
-                1 -> tab.text = getString(R.string.read_complete)
-                2 -> tab.text = getString(R.string.want_to_read)
-                else -> null
-            }
-        }.attach()
+        //메인 필터 클릭 이벤트
+        binding.filterRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            mainFilterViewModel.selectedFilter.value = checkedId
+        }
 
-        // Paging 완료되면 호출
+        //페이지 변환 후 호출되는 콜백 함수
         binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int){
+            override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
+                Log.e("ViewPagerFragment", "Page ${position+1}")
 
-                filterLayoutInit()
-                when(position) {    //읽고 싶은 탭에서는 북클럽 필터 버튼 GONE 으로
+                //완독 부분에선 북클럽 필터 GONE
+                when(position) {
                     0 -> setVisibilityClubButton(View.VISIBLE)
                     1 -> setVisibilityClubButton(View.VISIBLE)
                     2 -> setVisibilityClubButton(View.GONE)
@@ -60,67 +53,25 @@ class MyLibraryFragment : Fragment() {
             }
         })
 
-        binding.searchButton.setOnClickListener {
-            if (searchFlag==0) {
-                searchFlag = 1
-                clubFlag = 0
-                sortFlag = 0
-                fragmentManager!!.beginTransaction().replace(binding.filterLayout.id, SearchFragment()).commit()
-                binding.filterLayout.visibility = View.VISIBLE
-            } else {
-                searchFlag = 0
-                binding.filterRadioGroup.clearCheck()
-                binding.filterLayout.visibility = View.GONE
-            }
-            //Log.d("현재 플래그", "검색: ${searchFlag}, 북클럽: ${clubFlag}, 정렬: ${sortFlag}")
-        }
-
-        binding.clubButton.setOnClickListener {
-            if (clubFlag==0) {
-                searchFlag = 0
-                clubFlag = 1
-                sortFlag = 0
-                fragmentManager!!.beginTransaction().replace(binding.filterLayout.id, BookClubFilterFragment()).commit()
-                binding.filterLayout.visibility = View.VISIBLE
-            } else {
-                clubFlag = 0
-                binding.filterRadioGroup.clearCheck()
-                binding.filterLayout.visibility = View.GONE
-            }
-        }
-
-        binding.sortButton.setOnClickListener {
-            if (sortFlag==0) {
-                searchFlag = 0
-                clubFlag = 0
-                sortFlag = 1
-                fragmentManager!!.beginTransaction().replace(binding.filterLayout.id, SortFilterFragment()).commit()
-                binding.filterLayout.visibility = View.VISIBLE
-            } else {
-                sortFlag = 0
-                binding.filterRadioGroup.clearCheck()
-                binding.filterLayout.visibility = View.GONE
-            }
-        }
-
-        //val sp: Float = 10 / resources.displayMetrics.scaledDensity
-//        val dp: Float = 3 / resources.displayMetrics.density
-//        Log.d("dp", dp.toString())
-
         return binding.root
     }
 
-    private fun filterLayoutInit() {
-        searchFlag = 0
-        clubFlag = 0
-        sortFlag = 0
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        binding.filterLayout.removeAllViewsInLayout()
-        binding.filterLayout.visibility = View.GONE
-        binding.filterRadioGroup.clearCheck()
+        TabLayoutMediator(binding.readTypeTabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.reading)
+                1 -> tab.text = getString(R.string.read_complete)
+                2 -> tab.text = getString(R.string.want_to_read)
+                else -> null
+            }
+        }.attach()
     }
 
     private fun setVisibilityClubButton(visibility: Int) {
         binding.clubButton.visibility = visibility
     }
+
 }
+
