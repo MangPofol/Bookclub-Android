4baa8b091c1498c711e0eb80a1da9f6827585c99
EunhaKyeong <kyeongeh424@gmail.com>
2021-08-22 00:24:29 +0900

    Refactor: 변경 사용하지 않는 임포트된 클래스 삭제

--- app/src/main/java/com/example/bookclub/MainActivity.kt
+++ app/src/main/java/com/example/bookclub/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bookclub.adapter.BottomNavigationPagerAdapter
 import com.example.bookclub.databinding.ActivityMainBinding
+import com.example.bookclub.fragment.BookListFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/com/example/bookclub/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/bookclub/fragment/SearchFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.bookclub.R
 import com.example.bookclub.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
