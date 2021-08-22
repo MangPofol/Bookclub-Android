9d52be84f79e402c383eb14f65abafd1a318662d
EunhaKyeong <kyeongeh424@gmail.com>
2021-08-22 16:04:29 +0900

    Feat: 추가-정렬 서브 필터 체크 리스너 등록

--- app/src/main/java/com/example/bookclub/fragment/SortFilterFragment.kt
+++ app/src/main/java/com/example/bookclub/fragment/SortFilterFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.fragment.app.Fragment
+import com.example.bookclub.R
 import com.example.bookclub.databinding.FragmentSortFilterBinding
 
 class SortFilterFragment : Fragment() {
@@ -17,6 +19,34 @@ class SortFilterFragment : Fragment() {
     ): View? {
         binding = FragmentSortFilterBinding.inflate(inflater, container, false)
 
+        //체크박스 체크 리스너
+        val sortFilterListener = CompoundButton.OnCheckedChangeListener { checkBox, isChecked ->
+            if (isChecked)
+                setCheckedState(checkBox.id)
+        }
+
+        //체크박스 리스너 등록
+        binding.latestOrder.setOnCheckedChangeListener(sortFilterListener)
+        binding.oldOrder.setOnCheckedChangeListener(sortFilterListener)
+        binding.nameOrder.setOnCheckedChangeListener(sortFilterListener)
+
         return binding.root
+    }
+
+    private fun setCheckedState(checkBoxId: Int) {
+        when (checkBoxId) {
+            R.id.latestOrder -> {
+                binding.oldOrder.isChecked = false
+                binding.nameOrder.isChecked = false
+            }
+            R.id.oldOrder -> {
+                binding.latestOrder.isChecked = false
+                binding.nameOrder.isChecked = false
+            }
+            R.id.nameOrder -> {
+                binding.latestOrder.isChecked = false
+                binding.oldOrder.isChecked = false
+            }
+        }
     }
 }
