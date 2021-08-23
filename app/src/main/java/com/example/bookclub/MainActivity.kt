09bb269a24bdae09a52670a8b2e532d4baa551ed
EunhaKyeong <kyeongeh424@gmail.com>
2021-08-23 15:58:47 +0900

    Feat: 추가-onBackPressed 케이스 추가
    뒤로 가기를 눌렀을 때 내 서재 화면에서 북클럽 선택하는 화면이 보이고 있는 경우에는 gone 상태로 변경

--- app/src/main/java/com/example/bookclub/MainActivity.kt
+++ app/src/main/java/com/example/bookclub/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.bookclub
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.example.bookclub.adapter.BottomNavigationPagerAdapter
 import com.example.bookclub.databinding.ActivityMainBinding
 import com.example.bookclub.fragment.BookListFragment
+import com.example.bookclub.fragment.MyLibraryFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -23,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.selectedItemId = R.id.library
 
         binding.bottomNavigation.setOnItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.write -> {
                     binding.bottomViewPager.currentItem = 0
 
@@ -48,15 +51,30 @@ class MainActivity : AppCompatActivity() {
     fun setDrawer(toolbar: Toolbar) {
         var activity = MainActivity()
 
-        mDrawerToggle = ActionBarDrawerToggle(activity, binding.drawerLayout, toolbar, R.string.open, R.string.close)
+        mDrawerToggle = ActionBarDrawerToggle(
+            activity,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open,
+            R.string.close
+        )
         mDrawerToggle!!.syncState()
     }
 
     override fun onBackPressed() {
+        //view pager의 현재 프래그먼트 가져오기
+        var fragment: Fragment = binding.bottomViewPager.adapter!!.instantiateItem(
+            binding.bottomViewPager,
+            binding.bottomViewPager.currentItem
+        ) as Fragment
+
         //drawer layout이 열려 있는 상태면 -> drawer layout 부터 닫는다.
         if (binding.drawerLayout.isDrawerOpen(GravityCompat.START))
             binding.drawerLayout.closeDrawers()
-        else {
+        else if (fragment.javaClass == MyLibraryFragment().javaClass && (fragment as MyLibraryFragment).isClubListVisible()) {
+            //내 서재 화면의 클럽을 선택하는 top sheet 가 열려 있을 경우
+            (fragment as MyLibraryFragment).closeClubList()
+        } else {
             super.onBackPressed()
         }
 
