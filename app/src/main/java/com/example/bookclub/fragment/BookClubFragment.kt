d52f5aace4c0566828650a350fbaa98b293868ba
EunhaKyeong <kyeongeh424@gmail.com>
2021-08-24 23:19:28 +0900

    Feat: 추가-북클럽 화면 레이아웃 연결

---
+++ app/src/main/java/com/example/bookclub/fragment/BookClubFragment.kt
@@ -0,0 +1,27 @@
+package com.example.bookclub.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bookclub.R
+import com.example.bookclub.databinding.FragmentBookClubBinding
+
+class BookClubFragment : Fragment() {
+    private lateinit var binding: FragmentBookClubBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBookClubBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
