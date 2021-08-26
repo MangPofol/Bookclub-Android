cbd7adce8429799d63a555e9b28aa51dbe9a7d6a
EunhaKyeong <kyeongeh424@gmail.com>
2021-08-26 23:06:51 +0900

    Feat: 추가-북클럽 화면 이벤트 리스너 등록
    1. navigation drawer 연결
    2. 핫한 메모/핫한 토픽 클릭 리스너 등록

--- app/src/main/java/com/example/bookclub/fragment/BookClubFragment.kt
+++ app/src/main/java/com/example/bookclub/fragment/BookClubFragment.kt
@@ -1,10 +1,13 @@
 package com.example.bookclub.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.bookclub.MainActivity
 import com.example.bookclub.R
 import com.example.bookclub.databinding.FragmentBookClubBinding
 
@@ -21,7 +24,33 @@ class BookClubFragment : Fragment() {
     ): View? {
         binding = FragmentBookClubBinding.inflate(inflater, container, false)
 
+        binding.hotContents.check(binding.hotMemoButton.id)   //처음 선택된 라디오버튼: 핫한 메모
+        //라디오버튼 체크 이벤트 리스너
+        binding.hotContents.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                binding.hotMemoButton.id -> {   //핫한 메모 체크됐을 때
+                    for (index in 0 until binding.hotMemoContents.childCount) {
+                        (binding.hotMemoContents.getChildAt(index) as TextView).isEnabled = true
+                        (binding.hotTopicContents.getChildAt(index) as TextView).isEnabled = false
+                    }
+                }
+                binding.hotTopicButton.id -> {  //핫한 토픽 체크됐을 때
+                    for (index in 0 until binding.hotTopicContents.childCount) {
+                        (binding.hotTopicContents.getChildAt(index) as TextView).isEnabled = true
+                        (binding.hotMemoContents.getChildAt(index) as TextView).isEnabled = false
+                    }
+                }
+            }
+        }
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as MainActivity).setDrawer(binding.toolbar)   //navigation drawer 등록
+        binding.toolbar.setNavigationIcon(R.drawable.ic_baseline_more_vert_36)  //navigation icon 설정
     }
 
 }
