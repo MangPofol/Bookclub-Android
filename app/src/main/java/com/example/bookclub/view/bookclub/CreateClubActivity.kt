b5d38d8ac57c7521917f3c73a4e220dfe0d162d3
EunhaKyeong <kyeongeh424@gmail.com>
2021-09-13 01:28:39 +0900

    Feat: "추가 북클럽 생성화면에서 시스템 상단바 보이도록 설정"

--- app/src/main/java/com/example/bookclub/view/bookclub/CreateClubActivity.kt
+++ app/src/main/java/com/example/bookclub/view/bookclub/CreateClubActivity.kt
@@ -2,6 +2,7 @@ package com.example.bookclub.view.bookclub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.bookclub.databinding.ActivityCreateClubBinding
 
 class CreateClubActivity : AppCompatActivity() {
@@ -11,5 +12,12 @@ class CreateClubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateClubBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //시스템 툴바 보이도록
+        val window = window
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+        )
     }
 }
