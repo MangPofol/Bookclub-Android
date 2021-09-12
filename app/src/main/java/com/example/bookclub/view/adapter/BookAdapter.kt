6106a2cf45f2068c00a4b6e3aa4621c9d1e1bd14
EunhaKyeong <kyeongeh424@gmail.com>
2021-09-13 01:20:40 +0900

    Feat: "추가 BookAdapter 관련 기능 추가"
    - 생성자 추가
    - Glide를 활용해 이미지뷰에 책 이미지 바인딩, 책 제목 바인딩
    - 책 이미지 아이템 클릭 리스너 등록
    - 책 목록 데이터 변경하는 setBooks 함수 작성

--- app/src/main/java/com/example/bookclub/view/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/bookclub/view/adapter/BookAdapter.kt
@@ -1,17 +1,23 @@
 package com.example.bookclub.view.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.bookclub.databinding.BookItemBinding
-import com.example.bookclub.model.BookModel
+import com.example.bookclub.model.NaverBookModel
 
-class BookAdapter() : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+class BookAdapter() :
+    RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
     private lateinit var binding: BookItemBinding
-    private var books: List<BookModel> = listOf()
+    private lateinit var bookItemClick: OnBookItemClick
+    private var books: MutableList<NaverBookModel> = ArrayList<NaverBookModel>()
+
+    constructor(bookItemClick: OnBookItemClick): this() {
+        this.bookItemClick = bookItemClick
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookAdapter.BookViewHolder {
         binding = BookItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -21,14 +27,30 @@ class BookAdapter() : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
 
     override fun onBindViewHolder(holder: BookAdapter.BookViewHolder, position: Int) {
         holder.bookImg.clipToOutline = true
+        Glide.with(holder.bookImg)
+            .load(books[position].image)
+            .into(holder.bookImg)
+
+        holder.bookTitle.text = books[position].title
+
+        //책 이미지가 클릭됐을 때 리스너
+        holder.bookImg.setOnClickListener {
+            bookItemClick.onClick(holder.bookTitle.text.toString())
+        }
     }
 
     override fun getItemCount(): Int {
-        return 100
+        return books.size
     }
 
     inner class BookViewHolder(itemView: BookItemBinding) : RecyclerView.ViewHolder(itemView.root) {
         val bookImg: ImageView = itemView.bookIV
         val bookTitle: TextView = itemView.bookTitle
+
+    }
+
+    fun setBooks(bookList: MutableList<NaverBookModel>) {
+        books = bookList
+        notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/bookclub/view/adapter/OnBookItemClick.kt
@@ -0,0 +1,5 @@
+package com.example.bookclub.view.adapter
+
+interface OnBookItemClick {
+    fun onClick(title: String)
+}
