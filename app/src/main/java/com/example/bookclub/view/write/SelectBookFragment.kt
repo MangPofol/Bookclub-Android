11b472cc308030ed09ecf8ec256177681d2fc6b0
EunhaKyeong <kyeongeh424@gmail.com>
2021-09-13 01:28:12 +0900

    Feat: "추가 책 선택하기 화면에 필요한 기능 추가"
    - bookViewModel을 프래그먼트 간 공유할 수 있도록 변경
    - 책 추가할 때 bottomSheet 나타나도록 인스턴스 추가
    - bookViewModel의 books 변경되면 bookAdapter의 데이터가 변경되도록 하는 코드 추가
    - 리사이클러뷰에 어댑터 설정
    - 검색 edittext가 비어 있으면 리사이클러뷰도 invisible 되도록 코드 추가
    - OnBookItemClick 인터페이스 상속받아 onClick 메서드 오버라이드

--- app/src/main/java/com/example/bookclub/view/write/SelectBookFragment.kt
+++ app/src/main/java/com/example/bookclub/view/write/SelectBookFragment.kt
@@ -2,30 +2,34 @@ package com.example.bookclub.view.write
 
 import android.os.Bundle
 import android.text.Editable
-import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.bookclub.R
-import com.example.bookclub.databinding.FragmentRecordBinding
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.bookclub.databinding.FragmentSelectBookBinding
-import com.example.bookclub.viewmodel.SelectBookViewModel
+import com.example.bookclub.util.HorizontalItemDecorator
+import com.example.bookclub.util.VerticalItemDecorator
+import com.example.bookclub.view.MainActivity
+import com.example.bookclub.view.adapter.BookAdapter
+import com.example.bookclub.view.adapter.OnBookItemClick
+import com.example.bookclub.viewmodel.BookViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
-class SelectBookFragment : Fragment(), TextWatcher {
+class SelectBookFragment : Fragment(), TextWatcher, OnBookItemClick {
     private lateinit var binding: FragmentSelectBookBinding
-    private lateinit var selectBookViewModel: SelectBookViewModel
+    private lateinit var bookAdapter: BookAdapter
+    private val bookViewModel: BookViewModel by activityViewModels()
+    private val bottomSheet: AddBookBottomSheetFragment = AddBookBottomSheetFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        selectBookViewModel = ViewModelProvider(this)[SelectBookViewModel::class.java]
+        bookAdapter = BookAdapter(this)
     }
 
     override fun onCreateView(
@@ -34,8 +38,9 @@ class SelectBookFragment : Fragment(), TextWatcher {
     ): View? {
         binding = FragmentSelectBookBinding.inflate(inflater, container, false)
 
-        selectBookViewModel.books.observe(viewLifecycleOwner, Observer {
+        bookViewModel.books.observe(viewLifecycleOwner, Observer {
             Log.e("observe!!", it.toString())
+            bookAdapter.setBooks(it)
         })
 
         //책 검색 EditText에 TextChanged 리스너 등록
@@ -44,6 +49,12 @@ class SelectBookFragment : Fragment(), TextWatcher {
         binding.readTypeRG.setOnCheckedChangeListener { group, checkedId ->
 
         }
+
+        //Recycler View 어댑터 설정
+        binding.bookListRV.adapter = bookAdapter
+        binding.bookListRV.layoutManager = GridLayoutManager(this.context, 3)
+        binding.bookListRV.addItemDecoration(VerticalItemDecorator(50))
+        binding.bookListRV.addItemDecoration(HorizontalItemDecorator(20))
 
         return binding.root
     }
@@ -59,20 +70,30 @@ class SelectBookFragment : Fragment(), TextWatcher {
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         GlobalScope.launch {
-            selectBookViewModel.updateSearchBookTitle(s.toString())
+            bookViewModel.updateSearchBookTitle(s.toString())
         }
 
-        if (count != 0) {
+        if (s!!.isNotEmpty()) { //검색 테스트가 안 비어 있으면 recyclerview -> VISIBLE, radio group -> INVISIBLE
             binding.selectBookTV.visibility = View.INVISIBLE
             binding.readTypeRG.visibility = View.INVISIBLE
-        } else {
+            binding.bookListRV.visibility = View.VISIBLE
+        } else {    //검색 테스트가 비어 있으면 recyclerview -> INVISIBLE, radio group -> VISIBLE
             binding.selectBookTV.visibility = View.VISIBLE
             binding.readTypeRG.visibility = View.VISIBLE
-
+            binding.bookListRV.visibility = View.INVISIBLE
         }
     }
 
     override fun afterTextChanged(s: Editable?) {
 
+    }
+
+    override fun onClick(title: String) {
+        //책을 등록하는 bottom sheet 을 띄운다.
+        bottomSheet.show(
+            (activity as MainActivity).supportFragmentManager, bottomSheet.tag
+        )
+        //클릭된 책 제목을 SelectBookViewModel의 selectedBookTitle 변수에 업데이트
+        bookViewModel.updateSelectedTookTitle(title)
     }
 }
