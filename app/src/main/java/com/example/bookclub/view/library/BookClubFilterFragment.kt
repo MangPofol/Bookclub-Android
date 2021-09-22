31e8cc809eadeb01bed1a869060c486fc38f07f6
EunhaKyeong <kyeongeh424@gmail.com>
2021-09-23 00:42:12 +0900

    Design: 변경-사용자가 가입한 북클럽이 있을 때만 adapter 설정

--- app/src/main/java/com/example/bookclub/view/library/BookClubFilterFragment.kt
+++ app/src/main/java/com/example/bookclub/view/library/BookClubFilterFragment.kt
@@ -1,35 +1,58 @@
 package com.example.bookclub.view.library
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.bookclub.view.adapter.BookClubFilterAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.bookclub.databinding.FragmentBookClubFilterBinding
 import com.example.bookclub.model.ClubModel
 import com.example.bookclub.util.HorizontalItemDecorator
+import com.example.bookclub.view.adapter.BookClubFilterAdapter
+import com.example.bookclub.viewmodel.ClubViewModel
+import kotlinx.coroutines.*
 
 class BookClubFilterFragment : Fragment() {
     private lateinit var binding: FragmentBookClubFilterBinding
+    private lateinit var bookClubFilterAdapter: BookClubFilterAdapter
+
+    private val clubViewModel: ClubViewModel by activityViewModels<ClubViewModel>()
+
     private var clubs: List<ClubModel> = ArrayList<ClubModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.e("BookClubFilterFragment", "onCreate")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.e("BookClubFilterFragment", "onCreateView")
         binding = FragmentBookClubFilterBinding.inflate(inflater, container, false)
 
-        clubs = mutableListOf<ClubModel>(ClubModel(1L, "북클럽 A"), ClubModel(2L, "북클럽 B"))
+        CoroutineScope(Dispatchers.Main).launch {
+            clubViewModel.getClubsByUser()
 
-        binding.clubFilterRecyclerView.adapter = BookClubFilterAdapter(clubs)
+            if (clubViewModel.clubs.value!!.size==0) {
+                binding.clubFilterRecyclerView.removeAllViewsInLayout()
+            } else {
+                Log.e("BookClubFilterFragment", "clubs ${clubViewModel.clubs.value!!}")
+                bookClubFilterAdapter = BookClubFilterAdapter(clubViewModel.clubs.value!!)
+                binding.clubFilterRecyclerView.adapter = bookClubFilterAdapter
+            }
+        }
+
         binding.clubFilterRecyclerView.addItemDecoration(HorizontalItemDecorator(10))
 
         return binding.root
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.e("BookClubFilterFragment", "onDetach")
     }
 }
