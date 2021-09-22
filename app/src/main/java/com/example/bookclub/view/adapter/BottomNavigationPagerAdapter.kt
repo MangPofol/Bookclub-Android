ca1a82a5b9a871980140b81521e3a627c9397602
EunhaKyeong <kyeongeh424@gmail.com>
2021-09-23 00:43:44 +0900

    Style: 변경-사용하지 않는 코드 삭제

--- app/src/main/java/com/example/bookclub/view/adapter/BottomNavigationPagerAdapter.kt
+++ app/src/main/java/com/example/bookclub/view/adapter/BottomNavigationPagerAdapter.kt
@@ -1,13 +1,10 @@
 package com.example.bookclub.view.adapter
 
-import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.bookclub.view.bookclub.BookClubFragment
 import com.example.bookclub.view.library.MyLibraryFragment
-import com.example.bookclub.view.write.RecordFragment
-import com.example.bookclub.view.write.SelectBookFragment
 import com.example.bookclub.view.write.WriteFragment
 
 class BottomNavigationPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
