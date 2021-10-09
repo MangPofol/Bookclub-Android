0dcc5198aa73c33795df5226dbf39d830e4c0a60
EunhaKyeong <kyeongeh424@gmail.com>
2021-10-10 01:17:58 +0900

    Feat: 추가-책 검색 버튼 누르면 검색 화면 나타나도록

--- app/src/main/java/com/mangpo/bookclub/view/bookclub/BookClubFragment.kt
+++ app/src/main/java/com/mangpo/bookclub/view/bookclub/BookClubFragment.kt
@@ -9,23 +9,26 @@ import android.view.ViewGroup
 import android.widget.CompoundButton
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mangpo.bookclub.R
 import com.mangpo.bookclub.databinding.FragmentBookClubBinding
 import com.mangpo.bookclub.model.ClubModel
 import com.mangpo.bookclub.view.MainActivity
+import com.mangpo.bookclub.view.adapter.BookAdapter
 import com.mangpo.bookclub.view.adapter.HotMemoTopicViewpagerAdapter
+import com.mangpo.bookclub.view.library.SearchFragment
 import com.mangpo.bookclub.viewmodel.ClubViewModel
 import kotlinx.coroutines.*
 
 class BookClubFragment : Fragment() {
     private lateinit var binding: FragmentBookClubBinding
     private lateinit var bottomSheet: ClubSelectBottomSheetFragment
+    private lateinit var job: Deferred<Unit>
 
     private val clubViewModel: ClubViewModel by activityViewModels<ClubViewModel>()
     private val tabInfomation: ArrayList<String> = arrayListOf("핫한 메모", "핫한 토픽")
-
-    private lateinit var job: Deferred<Unit>
+    private val bookAdapter = BookAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,6 +86,9 @@ class BookClubFragment : Fragment() {
         val checkBoxListener = CompoundButton.OnCheckedChangeListener { checkBox, isChecked ->
             if (isChecked) {
                 changeFilterCheckBox(checkBox.id)
+            } else {
+                Log.e("BookClub", "체크 해제")
+                binding.bookFilterFrameLayout.removeAllViews()
             }
         }
 
@@ -90,6 +96,10 @@ class BookClubFragment : Fragment() {
         binding.searchButton.setOnCheckedChangeListener(checkBoxListener)
         binding.clubMemberButton.setOnCheckedChangeListener(checkBoxListener)
         binding.sortButton.setOnCheckedChangeListener(checkBoxListener)
+
+        //bookAdapter setting
+        binding.bookClubBookRv.adapter = bookAdapter
+        binding.bookClubBookRv.layoutManager = GridLayoutManager(requireContext(), 3)
 
         //selectedClubIdx가 변경되면 이에 따라 클럽 정보 다시 셋팅
         clubViewModel.selectedClubIdx.observe(viewLifecycleOwner, Observer {
@@ -107,12 +117,15 @@ class BookClubFragment : Fragment() {
         binding.toolbar.setNavigationIcon(R.drawable.ic_baseline_more_vert_36_white)  //navigation icon 설정
     }
 
-    //체크된 필터 말고는 모두 체크 해제
+    //책 필터 체크 리스너
     private fun changeFilterCheckBox(checkBoxId: Int) {
         when (checkBoxId) {
             binding.searchButton.id -> {
                 binding.clubMemberButton.isChecked = false
                 binding.sortButton.isChecked = false
+                childFragmentManager.beginTransaction()
+                    .replace(R.id.book_filter_frame_layout, SearchFragment(bookAdapter))
+                    .commitAllowingStateLoss()
             }
             binding.clubMemberButton.id -> {
                 binding.searchButton.isChecked = false
