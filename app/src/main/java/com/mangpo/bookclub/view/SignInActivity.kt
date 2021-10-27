c4f40d372857d680e3b2c8b9a3406175487d0e07
EunhaKyeong <kyeongeh424@gmail.com>
2021-10-27 14:38:31 +0900

    Feat: 추가-완료 버튼 누르면 책 프로필 설정 화면으로 이동

--- app/src/main/java/com/mangpo/bookclub/view/SignInActivity.kt
+++ app/src/main/java/com/mangpo/bookclub/view/SignInActivity.kt
@@ -1,8 +1,10 @@
 package com.mangpo.bookclub.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mangpo.bookclub.databinding.ActivitySignInBinding
+import com.mangpo.bookclub.view.book_profile.BookProfileInitActivity
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
@@ -16,6 +18,11 @@ class SignInActivity : AppCompatActivity() {
 
         binding.signinToolbar.setNavigationOnClickListener {    //뒤로가기 클릭 -> 액티비티 종료
             finish()
+        }
+
+        binding.signinCompleteTv.setOnClickListener {
+            val intent: Intent = Intent(this, BookProfileInitActivity::class.java)
+            startActivity(intent)
         }
     }
 }
