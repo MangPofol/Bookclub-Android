f965b21e51c84716b246f570abe2c453a4890fc2
EunhaKyeong <kyeongeh424@gmail.com>
2021-10-27 18:51:58 +0900

    Feat: 추가-다음 버튼 누르면 닉네임 설정 프래그먼트로, 다음 버튼 활성화/비활성화 기능

--- app/src/main/java/com/mangpo/bookclub/view/book_profile/BookProfileInitActivity.kt
+++ app/src/main/java/com/mangpo/bookclub/view/book_profile/BookProfileInitActivity.kt
@@ -2,7 +2,10 @@ package com.mangpo.bookclub.view.book_profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.mangpo.bookclub.R
 import com.mangpo.bookclub.databinding.ActivityBookProfileInitBinding
+import gun0912.tedimagepicker.util.ToastUtil.context
 
 class BookProfileInitActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBookProfileInitBinding
@@ -18,6 +21,27 @@ class BookProfileInitActivity : AppCompatActivity() {
 
         binding.nextBtn.outlineProvider = null  //다음 버튼 그림자 제거
 
+        //frmame layout의 초기 화면 설정
         supportFragmentManager.beginTransaction().replace(binding.frameLayout.id, BookProfileDescFragment()).commit()
+
+        binding.nextBtn.setOnClickListener {
+            when (supportFragmentManager.fragments[0].javaClass) {
+                BookProfileDescFragment::class.java -> {
+                    supportFragmentManager.beginTransaction().replace(binding.frameLayout.id, SetNicknameFragment()).commit()
+                }
+            }
+        }
+    }
+
+    fun unEnableNextBtn() {
+        binding.nextBtn.isEnabled = false
+        binding.nextBtn.setTextColor(ContextCompat.getColor(context, R.color.grey12))
+        binding.nextBtn.setBackgroundColor(ContextCompat.getColor(context, R.color.grey13))
+    }
+
+    fun enableNextBtn() {
+        binding.nextBtn.isEnabled = true
+        binding.nextBtn.setTextColor(ContextCompat.getColor(context, R.color.white))
+        binding.nextBtn.setBackgroundColor(ContextCompat.getColor(context, R.color.light_red))
     }
 }
