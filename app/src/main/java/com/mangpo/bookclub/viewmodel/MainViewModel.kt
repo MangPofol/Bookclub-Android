7fab164120974a444d20333804f2a2bb19ed5e36
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-04 01:50:38 +0900

    Feat: 추가-라이브데이터

--- app/src/main/java/com/mangpo/bookclub/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/mangpo/bookclub/viewmodel/MainViewModel.kt
@@ -1,12 +1,23 @@
 package com.mangpo.bookclub.viewmodel
 
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.gson.JsonObject
 import com.mangpo.bookclub.repository.UserRepository
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainViewModel(private val repository: UserRepository): ViewModel() {
+
+    private val _loginCode = MutableLiveData<Int>()
+    private val _emailAlertVisibility = MutableLiveData<Int>()
+
+    val loginCode: LiveData<Int> get() = _loginCode
+    val emailAlertVisibility: LiveData<Int> get() = _emailAlertVisibility
+
     /*private val userRepository: UserRepository = UserRepository()
 
     fun getUser(user: HashMap<String, Any>): UserModel {
@@ -23,13 +34,22 @@ class MainViewModel(private val repository: UserRepository): ViewModel() {
         Log.e("viewModel-로그아웃", result.toString())
     }*/
 
-    suspend fun login(user: JsonObject): Int =
-        withContext(viewModelScope.coroutineContext) {
-            repository.login(user)
+    suspend fun login(user: JsonObject) {
+        viewModelScope.launch {
+            _loginCode.value = repository.login(user)
         }
+    }
 
-    suspend fun validateEmail(email: JsonObject): Int =
-        withContext(viewModelScope.coroutineContext) {
+
+    suspend fun validateEmail(email: JsonObject) {
+        val statusCode = withContext(viewModelScope.coroutineContext) {
             repository.validateEmail(email)
         }
+
+        if (statusCode==204)
+            _emailAlertVisibility.value = View.INVISIBLE
+        else
+            _emailAlertVisibility.value = View.VISIBLE
+    }
+
 }
