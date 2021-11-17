1e224b37a495287a262a27840f314b527a851813
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-17 23:42:44 +0900

    Feat: 추가-기록 추가, 이미지 업로드 ViewModel 코드 작성

--- app/src/main/java/com/mangpo/bookclub/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/mangpo/bookclub/viewmodel/PostViewModel.kt
@@ -4,15 +4,22 @@ import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mangpo.bookclub.model.PostDetailModel
+import com.mangpo.bookclub.model.PostModel
+import com.mangpo.bookclub.repository.PostRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 
-class PostViewModel(): ViewModel() {
+class PostViewModel(private val repository: PostRepository) : ViewModel() {
     private val _imgs: MutableLiveData<List<Bitmap>> = MutableLiveData()
 
-    private var title: String? = null
-    private var content: String? = null
-    private var bookId: Long? = null
+    private var post: PostModel? = null
+    private var postDetail: PostDetailModel? = null
 
     val imgs: LiveData<List<Bitmap>> get() = _imgs
+
 
     fun setImgs(bitmaps: List<Bitmap>) {
         _imgs.value = bitmaps
@@ -22,83 +29,38 @@ class PostViewModel(): ViewModel() {
         (_imgs.value as ArrayList).removeAt(position)
     }
 
-    fun setTitle(title: String?) {
-        this.title = title
+    fun clearImg() {
+        if (_imgs.value != null)
+            (_imgs.value as ArrayList).clear()
     }
 
-    fun getTitle(): String? = title
-
-    fun setContent(content: String?) {
-        this.content = content
+    fun setPost(post: PostModel?) {
+        this.post = post
     }
 
-    fun getContent(): String? = content
+    fun getPost(): PostModel? = this.post
 
-    fun setBookId(bookId: Long?) {
-        this.bookId = bookId
-    }
+    fun getPostDetail(): PostDetailModel? = this.postDetail
 
-    fun getBookId(): Long? = this.bookId
-    /*private val postRepository: PostRespository = PostRespository()
-    private val fileRepository: FileRepository = FileRepository()
-
-    private val _imgUriList: MutableLiveData<MutableList<Uri>?> =
-        MutableLiveData<MutableList<Uri>?>()  //사용자가 기록하기에 넣을 이미지
-    private val _temporaryPost: MutableLiveData<PostReqModel?> = MutableLiveData<PostReqModel?>()   //임시로 저장해 놓는 post
-
-    val imgUriList: MutableLiveData<MutableList<Uri>?> get() = _imgUriList
-    val temporaryPost: MutableLiveData<PostReqModel?> get() = _temporaryPost
-
-    fun updateImgUriList(uriList: MutableList<Uri>?) {
-        _imgUriList.postValue(uriList)
-    }
-
-    fun initTemtporaryPost() {
-        _temporaryPost.value = PostReqModel()
-    }
-
-    fun setTemporaryPost(post: PostReqModel?) {
-        _temporaryPost.value = post
-    }*/
-
-    /*suspend fun updateBooksLatestPostDate(books: MutableList<BookModel>) {
-        for (book in books) {
-            if (book.latestPostDate=="") {
-                val posts = viewModelScope.async {
-                    getPost(book.id!!.toInt(), null)
-                }
-
-                //Log.e("PostViewModel", "updateBooksLatestPostDate\n book name: ${book.name}\n posts: ${posts.await()}")
-
-                if (posts.await()!!.size!=0)
-                    book.latestPostDate = posts.await()!![posts.await()!!.size-1].modifiedDate
-                else
-                    book.latestPostDate = book.modifiedDate
-            }
+    suspend fun createPost(post: PostModel): PostDetailModel? {
+        val job = viewModelScope.async(Dispatchers.Main) {
+            repository.createPost(post)
         }
-    }*/
 
-    /*suspend fun createPost(newPost: PostReqModel): PostModel? {
+        postDetail = job.await()
+
+        return postDetail
+    }
+
+    suspend fun uploadImg(imgPath: String): String? {
         return withContext(viewModelScope.coroutineContext + Dispatchers.IO) {
-            postRepository.createPost(newPost)
+            repository.uploadImgFile(imgPath)
         }
     }
 
-    suspend fun getPost(bookId: Int, clubId: Int?): ArrayList<PostModel>? {
+    suspend fun uploadMultiImg(imgPaths: List<String>): List<String>? {
         return withContext(viewModelScope.coroutineContext + Dispatchers.IO) {
-            postRepository.getPost(bookId, clubId)
+            repository.uploadMultiImgFile(imgPaths)
         }
     }
-
-    fun uploadMultipleImg(paths: List<String>) {
-        viewModelScope.launch(Dispatchers.IO) {
-            fileRepository.uploadMultipleFiles(paths)
-        }
-    }
-
-    suspend fun uploadImg(path: String): MutableList<String> {
-        return withContext(viewModelScope.coroutineContext + Dispatchers.IO) {
-            fileRepository.uploadFile(path)
-        }
-    }*/
 }
