9e09b6a0d7da6391c6754ccc2ee776177b9713ba
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-17 23:40:45 +0900

    Feat: 추가-Post 데이터 클래스 작성

--- app/src/main/java/com/mangpo/bookclub/model/PostModel.kt
+++ app/src/main/java/com/mangpo/bookclub/model/PostModel.kt
@@ -1,47 +1,29 @@
 package com.mangpo.bookclub.model
 
-import com.google.gson.annotations.SerializedName
-
-/*"id": 9,
-"type": "MEMO",
-"scope": "PUBLIC",
-"isIncomplete": true,
-"imgLocation": "123somewhere321",
-"title": "죽과를 읽고",
-"content": "다시 생각하니 노잼이였다",
-"createdDate": "2021-08-19T16:53:11",
-"modifiedDate": "2021-08-20T16:43:43",
-"likedList": [
-{
-    "userNickname": "rabbit",
-    "isLiked": true
-}
-],
-"commentsDto": [
-{
-    "userNickname": "rabbit",
-    "content": "this is content2.",
-    "createdDate": "2021-08-31T15:37:21",
-    "modifiedDate": "2021-08-31T15:37:21"
-},
-{
-    "userNickname": "rabbit",
-    "content": "this is content3.",
-    "createdDate": "2021-08-31T15:37:54",
-    "modifiedDate": "2021-08-31T15:37:54"
-}
-]*/
+/*{
+    "bookId": 290,
+    "scope": "CLUB",
+    "isIncomplete": false,
+    "location": "내 방",
+    "readTime": "해질녘",
+    "hyperlink": "www.mangpo.com",
+    "title": "죽과죽과",
+    "content": "꿀잼10",
+    "postImgLocations": [
+    "image3",
+    "image4"
+    ],
+    "clubIdListForScope": [6]
+}*/
 data class PostModel(
-    @SerializedName(value="id")
     var bookId: Long? = null,
-    var type: String = "",
-    var scope: String = "",
-    val isIncomplete: Boolean = false,
-    var imgLocation: String = "",
-    var title: String = "",
-    var content: String = "",
-    val createdDate: String = "",
-    val modifiedDate: String = "",
-    val likedList: ArrayList<LikedModel> = ArrayList<LikedModel>(),
-    val commentsDto: ArrayList<CommentModel> = ArrayList<CommentModel>()
+    var scope: String? = null,
+    var isIncomplete: Boolean = false,
+    var location: String? = null,
+    var readTime: String? = null,
+    var hyperlink: String? = null,
+    var title: String? = null,
+    var content: String? = null,
+    var postImgLocations: List<String> = ArrayList<String>(),
+//    var clubIdListForScope: List<Long> = ArrayList<Long>()
 )
