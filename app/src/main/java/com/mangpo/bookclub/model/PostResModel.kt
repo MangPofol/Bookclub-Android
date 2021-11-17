79fa0dc631e68371233f5b3ed987ec94ad6e816a
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-17 23:33:39 +0900

    Remove: 삭제-파일 삭제

--- app/src/main/java/com/mangpo/bookclub/model/PostResModel.kt
+++
@@ -1,8 +0,0 @@
-package com.mangpo.bookclub.model
-
-import com.google.gson.annotations.SerializedName
-
-data class PostResModel(
-    @SerializedName("data")
-    val posts: ArrayList<PostModel> = ArrayList<PostModel>()
-)
