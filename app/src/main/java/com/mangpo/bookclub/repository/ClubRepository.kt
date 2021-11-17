4e671fa303d263b85bf8ff642b403c3cdc114f6b
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-17 23:35:45 +0900

    Comment: 사용하지 않는 코드 주석 처리

--- app/src/main/java/com/mangpo/bookclub/repository/ClubRepository.kt
+++ app/src/main/java/com/mangpo/bookclub/repository/ClubRepository.kt
@@ -2,17 +2,18 @@ package com.mangpo.bookclub.repository
 
 import com.mangpo.bookclub.model.ClubModel
 import com.mangpo.bookclub.model.ClubResData
+import com.mangpo.bookclub.service.ApiClient
 import com.mangpo.bookclub.service.ClubService
 import retrofit2.Response
 
 class ClubRepository() {
     private val clubService: ClubService = ApiClient.clubService
 
-    suspend fun createClub(newClub: ClubModel): Response<ClubModel> {
+    /*suspend fun createClub(newClub: ClubModel): Response<ClubModel> {
         return clubService.createClub(newClub)
     }
 
     suspend fun getClubsByUser(): Response<ClubResData> {
         return clubService.getClubsByUser()
-    }
+    }*/
 }
