2cf294028b9989df5013d80e5105aa756b42218d
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-25 22:56:28 +0900

    Feat: hyperlinkTitle 멤버 변수 추가, 주석 코드 삭제

--- app/src/main/java/com/mangpo/bookclub/model/PostModel.kt
+++ app/src/main/java/com/mangpo/bookclub/model/PostModel.kt
@@ -1,26 +1,12 @@
 package com.mangpo.bookclub.model
 
-/*{
-    "bookId": 290,
-    "scope": "CLUB",
-    "isIncomplete": false,
-    "location": "내 방",
-    "readTime": "해질녘",
-    "hyperlink": "www.mangpo.com",
-    "title": "죽과죽과",
-    "content": "꿀잼10",
-    "postImgLocations": [
-    "image3",
-    "image4"
-    ],
-    "clubIdListForScope": [6]
-}*/
 data class PostModel(
     var bookId: Long? = null,
     var scope: String? = null,
     var isIncomplete: Boolean = false,
     var location: String? = null,
     var readTime: String? = null,
+    var hyperlinkTitle: String? = null,
     var hyperlink: String? = null,
     var title: String? = null,
     var content: String? = null,
