620be2023fd2ccc178531331ca0f1e7aca2b8dcc
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-25 22:56:02 +0900

    Feat: 이동하는 프래그먼트에 태그 추가, moveToPostDetail 메서드 로직 변경

--- app/src/main/java/com/mangpo/bookclub/view/write/PostFragment.kt
+++ app/src/main/java/com/mangpo/bookclub/view/write/PostFragment.kt
@@ -28,54 +28,14 @@ class PostFragment : Fragment() {
         binding = FragmentPostBinding.inflate(inflater, container, false)
 
         binding.frameLayout.removeAllViewsInLayout()
+
         //기본 화면을 기록하기 화면으로 설정
-        childFragmentManager.beginTransaction().add(binding.frameLayout.id, RecordFragment())
+        childFragmentManager.beginTransaction()
+            .add(binding.frameLayout.id, RecordFragment(), "record")
             .addToBackStack("record")
             .commit()
 
         return binding.root
-    }
-
-    private fun moveToRecord() {
-        childFragmentManager.beginTransaction()
-            .replace(binding.frameLayout.id, RecordFragment()).addToBackStack("record").commit()
-    }
-
-    fun moveToSelectBook() {
-        binding.frameLayout.removeAllViews()
-        childFragmentManager.beginTransaction()
-            .replace(binding.frameLayout.id, SelectFragment()).addToBackStack("selectBook").commit()
-    }
-
-    fun moveToWritingSetting() {
-        binding.frameLayout.removeAllViews()
-        childFragmentManager.beginTransaction()
-            .replace(binding.frameLayout.id, WritingSettingFragment())
-            .addToBackStack("writingSetting").commit()
-    }
-
-    /*supportFragmentManager.beginTransaction().replace(
-    R.id.fragment_my_container,
-    MyFragment().apply {
-        arguments = Bundle().apply {
-            putString("KEY", "value")
-        }
-    }
-    ).commit()*/
-
-    fun moveToPostDetail() {
-        binding.frameLayout.removeAllViews()
-
-        childFragmentManager.beginTransaction()
-            .replace(binding.frameLayout.id, PostDetailFragment().apply {
-                        arguments = Bundle().apply {
-                            putLong("bookId", bookVm.getSelectedBook()!!.id!!)
-                            putString("bookName", bookVm.getSelectedBook()!!.name)
-                        }
-            })
-            .addToBackStack("postDetail").commit()
-
-        bookVm.setSelectedBook(null)
     }
 
     override fun onPause() {
@@ -84,4 +44,58 @@ class PostFragment : Fragment() {
 
         moveToRecord()
     }
+
+    fun moveToRecord() {
+        childFragmentManager.beginTransaction()
+            .replace(binding.frameLayout.id, RecordFragment(), "record").addToBackStack("record")
+            .commit()
+    }
+
+    fun moveToSelectBook() {
+        binding.frameLayout.removeAllViews()
+        childFragmentManager.beginTransaction()
+            .replace(binding.frameLayout.id, SelectFragment(), "select_book")
+            .addToBackStack("selectBook").commit()
+    }
+
+    fun moveToWritingSetting() {
+        binding.frameLayout.removeAllViews()
+        childFragmentManager.beginTransaction()
+            .replace(binding.frameLayout.id, WritingSettingFragment(), "writing_setting")
+            .addToBackStack("writingSetting").commit()
+    }
+
+    fun moveToPostDetail(bookId: Long?, bookName: String?) {
+        binding.frameLayout.removeAllViews()
+
+        var id: Long? = null
+        var name: String? = null
+
+        if (bookId == null) {
+            id = bookVm.getSelectedBook()!!.id!!
+            name = bookVm.getSelectedBook()!!.name!!
+            bookVm.setSelectedBook(null)
+        } else {
+            id = bookId
+            name = bookName
+        }
+
+        childFragmentManager.beginTransaction()
+            .replace(binding.frameLayout.id, PostDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putLong("bookId", id)
+                    putString("bookName", name)
+                }
+            }, "post_detail")
+            .addToBackStack("postDetail").commit()
+    }
+
+    /*supportFragmentManager.beginTransaction().replace(
+   R.id.fragment_my_container,
+   MyFragment().apply {
+       arguments = Bundle().apply {
+           putString("KEY", "value")
+       }
+   }
+   ).commit()*/
 }
