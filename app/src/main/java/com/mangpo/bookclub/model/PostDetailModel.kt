69722c088eb63e02870184fb8772e2646adf09ea
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-25 22:53:52 +0900

    Feat: hyperlinkTitle에 변수 추가, 주석 삭제

--- app/src/main/java/com/mangpo/bookclub/model/PostDetailModel.kt
+++ app/src/main/java/com/mangpo/bookclub/model/PostDetailModel.kt
@@ -1,24 +1,9 @@
 package com.mangpo.bookclub.model
 
-/*{
-    "id":523,
-    "scope":"PRIVATE",
-    "isIncomplete":false,
-    "imgLocation":null,
-    "title":"돌이킬 수 없는 약속",
-    "content":"글 설정 데이터 추가 테스트",
-    "createdDate":"2021-11-17T13:27:34.39063",
-    "modifiedDate":"2021-11-17T13:27:34.39063",
-    "location":"우리집",
-    "readTime":"밤",
-    "hyperlink":"https://www.naver.com",
-    "postImgLocations":[
-        "https://elasticbeanstalk-ap-northeast-2-081654662783.s3.ap-northeast-2.amazonaws.com/static/c2a06f4c-79e6-41f0-8d10-28ab518539f6_1637155593405"
-    ],
-    "postScopeClub":{},
-    "likedList":[],
-    "commentsDto":[]
-}*/
+data class AllPostModel(
+    var data: List<PostDetailModel>? = null
+)
+
 data class PostDetailModel(
     var id: Long,
     var scope: String,
@@ -30,6 +15,7 @@ data class PostDetailModel(
     var modifiedDate: String,
     var location: String?,
     var readTime: String?,
+    var hyperlinkTitle: String?,
     var hyperlink: String?,
     var postImgLocations: List<String> = ArrayList<String>(),
     var postScopeClub: ClubModel? = null,
