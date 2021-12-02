38014063360055ba8ce35553aef9e2e3c52f4f1c
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-02 23:28:57 +0900

    Feat: 추가 - 수정하기 버튼 클릭 이벤트 추가, 사진 클릭 시 PhotoViewActivity로 이동하도록 코드 작성

--- app/src/main/java/com/mangpo/bookclub/view/write/PostDetailFragment.kt
+++ app/src/main/java/com/mangpo/bookclub/view/write/PostDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.mangpo.bookclub.view.write
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,14 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.mangpo.bookclub.databinding.FragmentPostDetailBinding
+import com.mangpo.bookclub.view.main.MainActivity
 import com.mangpo.bookclub.viewmodel.PostViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import java.util.ArrayList
 
 class PostDetailFragment : Fragment() {
 
     private val postVm: PostViewModel by sharedViewModel()
 
-    private var bookId: Long? = null
+    private var isInit: Boolean = false
 
     private lateinit var binding: FragmentPostDetailBinding
 
@@ -32,19 +35,55 @@ class PostDetailFragment : Fragment() {
         binding = FragmentPostDetailBinding.inflate(inflater, container, false)
         Log.d("PostDetailFragment", "onCreateView")
 
-        bookId = arguments?.getLong("bookId")
-
         setUI()
 
         binding.backIv.setOnClickListener {
-            parentFragmentManager.popBackStackImmediate()
+            requireActivity().supportFragmentManager.popBackStackImmediate()
+
+            if (isInit) {
+                postVm.setPostDetail(null)
+                arguments?.remove("bookId")
+                arguments?.remove("bookName")
+            }
+        }
+
+        binding.updateTv.setOnClickListener {
+            isInit = false
+            (requireParentFragment() as PostFragment).moveToRecord()
         }
 
         return binding.root
     }
 
+    override fun onStop() {
+        super.onStop()
+        Log.d("PostDetailFragment", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("PostDetailFragment", "onDestroyView")
+
+        if (isInit) {
+            postVm.setPostDetail(null)
+            arguments?.remove("bookId")
+            arguments?.remove("bookName")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("PostDetailFragment", "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("PostDetailFragment", "onDetach")
+    }
+
     private fun setUI() {
         val post = postVm.getPostDetail()!!
+        Log.d("PostDetailFragment", "post: $post")
 
         binding.bookNameTv.text = arguments?.getString("bookName")
 
@@ -57,11 +96,10 @@ class PostDetailFragment : Fragment() {
         binding.recordContentTv.text = post.content
         setImg(post.postImgLocations)
 
-
         binding.locationTv.text = post.location
         binding.clockTv.text = post.readTime
-        binding.likeCntTv.text = post.likedList.size.toString()
-        binding.commentCntTv.text = post.commentsDto.size.toString()
+        /*binding.likeCntTv.text = post.likedList.size.toString()
+        binding.commentCntTv.text = post.commentsDto.size.toString()*/
     }
 
     private fun setImg(imgs: List<String>) {
@@ -117,17 +155,37 @@ class PostDetailFragment : Fragment() {
 
     private fun setVisibilityOneIV(visibility: Int) {
         binding.oneIv.visibility = visibility
+        binding.oneIv.setOnClickListener {
+            goPhotoViewActivity(0)
+        }
     }
 
     private fun setVisibilityTwoIV(visibility: Int) {
         binding.twoIv1.visibility = visibility
         binding.twoIv2.visibility = visibility
+
+        binding.twoIv1.setOnClickListener {
+            goPhotoViewActivity(0)
+        }
+        binding.twoIv2.setOnClickListener {
+            goPhotoViewActivity(1)
+        }
     }
 
     private fun setVisibilityThreeIV(visibility: Int) {
         binding.threeIv1.visibility = visibility
         binding.threeIv2.visibility = visibility
         binding.threeIv3.visibility = visibility
+
+        binding.threeIv1.setOnClickListener {
+            goPhotoViewActivity(0)
+        }
+        binding.threeIv2.setOnClickListener {
+            goPhotoViewActivity(1)
+        }
+        binding.threeIv3.setOnClickListener {
+            goPhotoViewActivity(2)
+        }
     }
 
     private fun setVisibilityFourIV(visibility: Int) {
@@ -135,6 +193,30 @@ class PostDetailFragment : Fragment() {
         binding.fourIv2.visibility = visibility
         binding.fourIv3.visibility = visibility
         binding.fourIv4.visibility = visibility
+
+        binding.fourIv1.setOnClickListener {
+            goPhotoViewActivity(0)
+        }
+        binding.fourIv2.setOnClickListener {
+            goPhotoViewActivity(1)
+        }
+        binding.fourIv3.setOnClickListener {
+            goPhotoViewActivity(2)
+        }
+        binding.fourIv4.setOnClickListener {
+            goPhotoViewActivity(3)
+        }
+    }
+
+    private fun goPhotoViewActivity(position: Int) {
+        isInit = false
+
+        val intent: Intent = Intent(requireContext(), PhotoViewActivity::class.java)
+        intent.putStringArrayListExtra("imgs", postVm.getPostDetail()!!.postImgLocations as ArrayList<String>)
+        intent.putExtra("currentItem", position)
+        startActivity(intent)
+
+        (requireActivity() as MainActivity).setLatestFragment("PostDetail")
     }
 
 }
