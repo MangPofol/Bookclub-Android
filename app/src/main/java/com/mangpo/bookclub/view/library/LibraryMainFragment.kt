ed359c032659e8cde47560eef77df215965c3c10
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-02 23:19:19 +0900

    Feat: 변경 - commit -> commitAllowingStateLoss, 프래그먼트가 연결돼 있지 않을 때만 add

--- app/src/main/java/com/mangpo/bookclub/view/library/LibraryMainFragment.kt
+++ app/src/main/java/com/mangpo/bookclub/view/library/LibraryMainFragment.kt
@@ -1,6 +1,7 @@
 package com.mangpo.bookclub.view.library
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,25 +19,55 @@ class LibraryMainFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLibraryMainBinding.inflate(inflater, container, false)
 
-        childFragmentManager.beginTransaction()
-            .replace(binding.libraryMainFrameLayout.id, MyLibraryFragment())
-            .addToBackStack("myLibrary").commit()
+        if (childFragmentManager.fragments.size==0)
+            childFragmentManager.beginTransaction()
+                    .add(binding.libraryMainFrameLayout.id, MyLibraryFragment())
+                    .addToBackStack("myLibrary").commitAllowingStateLoss()
 
         return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LibraryMainFragment", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LibraryMainFragment", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LibraryMainFragment", "onDestroy")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("LibraryMainFragment", "onDestroyView")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("LibraryMainFragment", "onDetach")
+    }
+
     fun moveToBookDesc(book: BookModel) {
         childFragmentManager.beginTransaction()
-            .replace(binding.libraryMainFrameLayout.id, BookDescFragment().apply {
-                arguments = Bundle().apply {
-                    putString("book", Gson().toJson(book))
-                }
-            }).addToBackStack("bookDesc").commit()
+                .replace(binding.libraryMainFrameLayout.id, BookDescFragment().apply {
+                    arguments = Bundle().apply {
+                        putString("book", Gson().toJson(book))
+                    }
+                }).addToBackStack("bookDesc").commitAllowingStateLoss()
     }
 
 }
