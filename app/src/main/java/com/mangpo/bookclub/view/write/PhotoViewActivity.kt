d86371d23a1345a39ba737adbe97fe9d55cd59cd
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-05 18:53:16 +0900

    Feat: 추가-PhotoViewActivity 추가(PostDetailFragment 에서 사진 클릭하면 보이는 화면)

---
+++ app/src/main/java/com/mangpo/bookclub/view/write/PhotoViewActivity.kt
@@ -0,0 +1,30 @@
+package com.mangpo.bookclub.view.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.mangpo.bookclub.databinding.ActivityPhotoViewBinding
+import com.mangpo.bookclub.view.adapter.PhotoViewPagerAdapter
+
+class PhotoViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPhotoViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPhotoViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imgs = intent.getStringArrayListExtra("imgs")
+        val currentItem = intent.getIntExtra("currentItem", 0)
+
+        val adaptor: PhotoViewPagerAdapter = PhotoViewPagerAdapter(imgs!!)
+
+        binding.photoViewVp.adapter = adaptor
+        binding.photoViewVp.setCurrentItem(currentItem, true)
+        binding.photoViewIndicator.setViewPager2(binding.photoViewVp)
+
+        binding.exitIv.setOnClickListener {
+            finish()
+        }
+    }
+}
