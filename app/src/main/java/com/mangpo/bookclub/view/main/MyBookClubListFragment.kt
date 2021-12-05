19a562a89243224349cfffabf6bfdeccd0274f11
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-05 19:07:30 +0900

    Design: 추가-북클럽 관련 프래그먼트 디자인

---
+++ app/src/main/java/com/mangpo/bookclub/view/main/MyBookClubListFragment.kt
@@ -0,0 +1,35 @@
+package com.mangpo.bookclub.view.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mangpo.bookclub.R
+import com.mangpo.bookclub.databinding.FragmentMyBookClubListBinding
+import com.mangpo.bookclub.view.bookclub.CreateClubActivity
+
+class MyBookClubListFragment : Fragment() {
+
+    private lateinit var binding: FragmentMyBookClubListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMyBookClubListBinding.inflate(inflater, container, false)
+
+        binding.addBookClubBtn.setOnClickListener {
+            val intent: Intent = Intent(requireContext(), CreateClubActivity::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
+    }
+}
