150ef35d747fe7fe194fe3531c7d37a7d545243e
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-07 00:37:37 +0900

    Feat: 추가-아래 화살표 클릭 리스너(체크리스트 뷰 접기/펼치기)

--- app/src/main/java/com/mangpo/bookclub/view/write/WriteInitFragment.kt
+++ app/src/main/java/com/mangpo/bookclub/view/write/WriteInitFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mangpo.bookclub.R
 import com.mangpo.bookclub.databinding.FragmentWriteInitBinding
 import com.mangpo.bookclub.model.BookModel
 import com.mangpo.bookclub.model.PostModel
@@ -17,6 +16,10 @@ import com.mangpo.bookclub.viewmodel.PostViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.text.SimpleDateFormat
 import java.util.*
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
+import com.mangpo.bookclub.R
+import com.mangpo.bookclub.view.adapter.ChecklistRVAdapter
 
 class WriteInitFragment : Fragment() {
 
@@ -24,6 +27,7 @@ class WriteInitFragment : Fragment() {
 
     private val postVm: PostViewModel by sharedViewModel()
     private val bookVm: BookViewModel by sharedViewModel()
+    private val checklistAdapter: ChecklistRVAdapter = ChecklistRVAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +44,9 @@ class WriteInitFragment : Fragment() {
 
         binding = FragmentWriteInitBinding.inflate(layoutInflater, container, false)
 
+        //체크리스트 리사이클러뷰 어댑터 설정
+        binding.checklistHiddenRv.adapter = checklistAdapter
+
         //햄버거 메뉴 클릭 리스너 -> SettingActivity 화면 이동
         binding.hamburgerIb.setOnClickListener {
             val intent: Intent = Intent(requireContext(), SettingActivity::class.java)
@@ -54,6 +61,30 @@ class WriteInitFragment : Fragment() {
         //기록 쓰러가기 클릭 리스너 -> RecordFragment 로 이동
         binding.goPostView.setOnClickListener {
             (requireParentFragment() as WriteFrameFragment).moveToRecord(false)
+        }
+
+        //체크리스트의 arrow_down 이미지뷰 클릭 리스너
+        binding.arrowDownIv.setOnClickListener {
+            //체크리스트 목록 뷰가 열려 있으면 -> 닫기
+            if (binding.checklistHiddenRv.visibility == View.VISIBLE) {
+                //체크리스트 고정뷰의 모든 코너를 둥글게
+                binding.checklistView.setBackgroundResource(R.drawable.write_init_fragment_grey13_view)
+                //애니메이션
+                TransitionManager.beginDelayedTransition(
+                    binding.baseCardView,
+                    AutoTransition()
+                )
+                binding.checklistHiddenRv.visibility = View.GONE
+            } else {    //체크리스트 목록 뷰가 닫혀 있으면 -> 열기
+                //체크리스트 고정뷰의 위 모서리 코너를 둥글게
+                binding.checklistView.setBackgroundResource(R.drawable.write_init_fragment_grey13_top_round_view)
+                //애니메이션
+                TransitionManager.beginDelayedTransition(
+                    binding.baseCardView,
+                    AutoTransition()
+                )
+                binding.checklistHiddenRv.visibility = View.VISIBLE
+            }
         }
 
         return binding.root
