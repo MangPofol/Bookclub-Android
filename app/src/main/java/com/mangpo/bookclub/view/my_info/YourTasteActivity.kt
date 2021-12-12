da5a63980eed74a2bd966d0dabf2e23e43c491f8
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-12 13:10:52 +0900

    Design: 추가-당신의 취향 화면

---
+++ app/src/main/java/com/mangpo/bookclub/view/my_info/YourTasteActivity.kt
@@ -0,0 +1,17 @@
+package com.mangpo.bookclub.view.my_info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mangpo.bookclub.databinding.ActivityYourTasteBinding
+
+class YourTasteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityYourTasteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityYourTasteBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
