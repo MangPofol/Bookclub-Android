c337026e3320a3ce834a8199fa590716a66f8473
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-14 21:50:28 +0900

    Feat: 추가-JWT 저장

--- app/src/main/java/com/mangpo/bookclub/util/AccountSharedPreference.kt
+++ app/src/main/java/com/mangpo/bookclub/util/AccountSharedPreference.kt
@@ -5,35 +5,48 @@ import android.content.SharedPreferences
 
 object AccountSharedPreference {
 
-    private const val MY_ACCOUNT : String = "account"
+    private const val MY_ACCOUNT: String = "account"
+    private var token: String = ""
+
+    fun setJWT(token: String) {
+        this.token = token
+
+    }
+
+    fun getJWT(): String = token
 
     fun setUserEmail(context: Context, input: String) {
-        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        val editor : SharedPreferences.Editor = prefs.edit()
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
         editor.putString("MY_EMAIL", input)
         editor.commit()
     }
 
     fun getUserEmail(context: Context): String {
-        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
         return prefs.getString("MY_EMAIL", "").toString()
     }
 
     fun setUserPass(context: Context, input: String) {
-        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        val editor : SharedPreferences.Editor = prefs.edit()
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
         editor.putString("MY_PASS", input)
         editor.commit()
     }
 
     fun getUserPass(context: Context): String {
-        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
         return prefs.getString("MY_PASS", "").toString()
     }
 
     fun clearUser(context: Context) {
-        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
-        val editor : SharedPreferences.Editor = prefs.edit()
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
         editor.clear()
         editor.commit()
     }
