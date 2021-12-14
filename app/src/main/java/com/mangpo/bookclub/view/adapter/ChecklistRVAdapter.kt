020aa0064437b793d7881bbbf3270a8b9f71f560
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-14 22:17:18 +0900

    Feat: 변경-체크리스트 처음 추가할 때랑 수정할 때 나눠서 처리하려고 하는 중

--- app/src/main/java/com/mangpo/bookclub/view/adapter/ChecklistRVAdapter.kt
+++ app/src/main/java/com/mangpo/bookclub/view/adapter/ChecklistRVAdapter.kt
@@ -1,7 +1,10 @@
 package com.mangpo.bookclub.view.adapter
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mangpo.bookclub.R
@@ -11,7 +14,7 @@ class ChecklistRVAdapter() : RecyclerView.Adapter<ChecklistRVAdapter.ChecklistHo
     private lateinit var binding: ItemChecklistBinding
 
     private val checklist: ArrayList<String> =
-        arrayListOf("눌러서 작성하세요.", "눌러서 작성하세요.", "눌러서 작성하세요.", "눌러서 작성하세요.", "눌러서 작성하세요.")
+        arrayListOf("체크리스트1", "눌러서 작성하세요.", "눌러서 작성하세요.", "눌러서 작성하세요.", "눌러서 작성하세요.")
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -23,13 +26,36 @@ class ChecklistRVAdapter() : RecyclerView.Adapter<ChecklistRVAdapter.ChecklistHo
     }
 
     override fun onBindViewHolder(holder: ChecklistRVAdapter.ChecklistHolder, position: Int) {
-        holder.checklistTv.text = checklist[position]
+        holder.checklistEt.addTextChangedListener(ChecklistTextWatcher(position))
+
+        if (checklist[position]=="눌러서 작성하세요.") {
+        } else {
+            holder.checklistEt.setText(checklist[position])
+        }
+
     }
 
     override fun getItemCount(): Int = checklist.size
 
     inner class ChecklistHolder(itemView: ItemChecklistBinding) :
         RecyclerView.ViewHolder(itemView.root) {
-        val checklistTv: TextView = itemView.checklistTv
+        val checklistEt: EditText = itemView.checklistEt
+    }
+
+    inner class ChecklistTextWatcher(var position: Int): TextWatcher {
+        private var isCreate: Boolean = true
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            isCreate = s.toString()=="눌러서 작성하세요."
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
     }
 }
