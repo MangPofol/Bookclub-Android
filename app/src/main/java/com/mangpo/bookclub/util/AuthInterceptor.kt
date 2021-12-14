955f984deb2045ac2eca6dbe19d60d71c1f13840
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-14 22:57:55 +0900

    Feat: 추가-JWT 토큰 관련 인터셉터 추가

---
+++ app/src/main/java/com/mangpo/bookclub/util/AuthInterceptor.kt
@@ -0,0 +1,12 @@
+package com.mangpo.bookclub.util
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var req =
+            chain.request().newBuilder().addHeader("Authorization", "Bearer ${AccountSharedPreference.getJWT()}").build()
+        return chain.proceed(req)
+    }
+}
