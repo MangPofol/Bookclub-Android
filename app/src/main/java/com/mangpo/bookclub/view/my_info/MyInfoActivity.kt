b18895192ea29109bf6e380b7cd2907332ff5d80
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-14 22:33:32 +0900

    Feat: 추가-화면 이동 기능

--- app/src/main/java/com/mangpo/bookclub/view/my_info/MyInfoActivity.kt
+++ app/src/main/java/com/mangpo/bookclub/view/my_info/MyInfoActivity.kt
@@ -1,5 +1,6 @@
 package com.mangpo.bookclub.view.my_info
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mangpo.bookclub.databinding.ActivityMyInfoBinding
@@ -13,5 +14,40 @@ class MyInfoActivity : AppCompatActivity() {
         binding = ActivityMyInfoBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        binding.genreNextIv.setOnClickListener {
+            goYourTaste()
+        }
+
+        binding.styleNextIv.setOnClickListener {
+            goYourTaste()
+        }
+
+        binding.goalManagementIv.setOnClickListener {
+            goGoalManagement()
+        }
+
+        binding.checklistManagementIv.setOnClickListener {
+            goChecklistManagement()
+        }
+
+        binding.backIvView.setOnClickListener {
+            finish()
+        }
+    }
+
+    //ChecklistManagementActivity 화면으로 이동하는 함수
+    private fun goChecklistManagement() {
+        startActivity(Intent(this, ChecklistManagementActivity::class.java))
+    }
+
+    //GoalManagementActivity 화면으로 이동하는 함수
+    private fun goGoalManagement() {
+        startActivity(Intent(this, GoalManagementActivity::class.java))
+    }
+
+    //YourTasteActivity 화면으로 이동하는 함수
+    private fun goYourTaste() {
+        startActivity(Intent(this, YourTasteActivity::class.java))
     }
 }
