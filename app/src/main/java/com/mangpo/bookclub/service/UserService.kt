64f622efa47ea1efa6227c5ebf54d328e11573ce
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-14 22:55:10 +0900

    Feat: 변경-login API Uri 변경

--- app/src/main/java/com/mangpo/bookclub/service/UserService.kt
+++ app/src/main/java/com/mangpo/bookclub/service/UserService.kt
@@ -2,21 +2,14 @@ package com.mangpo.bookclub.service
 
 import com.google.gson.JsonObject
 import com.mangpo.bookclub.model.UserModel
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface UserService {
 
-    /*@Headers("Content-Type: application/json")
-    @POST("/users")
-    fun createUser(@Body user: HashMap<String, Any>): Call<UserModel>*/
-
-    @POST("/login")
-    suspend fun login(@Body user: JsonObject): Response<String>
+    @POST("/auth")
+    suspend fun login(@Body user: JsonObject): Response<JsonObject>
 
     @POST("/users/validate-duplicate")
     suspend fun validateEmail(@Body email: JsonObject): Response<JsonObject>
