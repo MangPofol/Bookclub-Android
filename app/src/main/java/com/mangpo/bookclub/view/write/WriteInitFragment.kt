59792e2bf12878b7e15d58f27b56d7832d030faf
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-19 01:27:47 +0900

    Feat: 추가-전체 기록수 조회해서 UI 업데이트 하는 기능

--- app/src/main/java/com/mangpo/bookclub/view/write/WriteInitFragment.kt
+++ app/src/main/java/com/mangpo/bookclub/view/write/WriteInitFragment.kt
@@ -8,10 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mangpo.bookclub.databinding.FragmentWriteInitBinding
-import com.mangpo.bookclub.model.BookModel
-import com.mangpo.bookclub.model.PostModel
 import com.mangpo.bookclub.view.SettingActivity
-import com.mangpo.bookclub.viewmodel.BookViewModel
 import com.mangpo.bookclub.viewmodel.PostViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.text.SimpleDateFormat
@@ -19,21 +16,34 @@ import java.util.*
 import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.mangpo.bookclub.R
-import com.mangpo.bookclub.view.adapter.ChecklistRVAdapter
+import com.mangpo.bookclub.model.CheckListModel
+import com.mangpo.bookclub.model.UserModel
+import com.mangpo.bookclub.view.adapter.ChecklistContentRVAdapter
 import com.mangpo.bookclub.view.main.MainActivity
+import com.mangpo.bookclub.viewmodel.ChecklistViewModel
+import com.mangpo.bookclub.viewmodel.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class WriteInitFragment : Fragment() {
 
     private lateinit var binding: FragmentWriteInitBinding
 
     private val postVm: PostViewModel by sharedViewModel()
-    private val bookVm: BookViewModel by sharedViewModel()
-    private val checklistAdapter: ChecklistRVAdapter = ChecklistRVAdapter()
+    private val mainVm: MainViewModel by sharedViewModel()
+    private val checklistVm: ChecklistViewModel by sharedViewModel()
+    private val checklistContentAdapter: ChecklistContentRVAdapter = ChecklistContentRVAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         Log.d("WriteInitFragment", "onCreate")
+    }
+
+    private fun getTotalPostCnt() {
+        CoroutineScope(Dispatchers.Main).launch {
+            postVm.getTotalPostCnt()
+        }
     }
 
     override fun onCreateView(
@@ -45,8 +55,8 @@ class WriteInitFragment : Fragment() {
 
         binding = FragmentWriteInitBinding.inflate(layoutInflater, container, false)
 
-        //체크리스트 리사이클러뷰 어댑터 설정
-        binding.checklistHiddenRv.adapter = checklistAdapter
+        initAdapter()
+        observe()
 
         //햄버거 메뉴 클릭 리스너 -> SettingActivity 화면 이동
         binding.hamburgerIb.setOnClickListener {
@@ -69,7 +79,7 @@ class WriteInitFragment : Fragment() {
             //체크리스트 목록 뷰가 열려 있으면 -> 닫기
             if (binding.checklistHiddenRv.visibility == View.VISIBLE) {
                 //체크리스트 고정뷰의 모든 코너를 둥글게
-                binding.checklistView.setBackgroundResource(R.drawable.write_init_fragment_grey13_view)
+                binding.checklistView.setBackgroundResource(R.drawable.grey13_round_view)
                 //애니메이션
                 TransitionManager.beginDelayedTransition(
                     binding.baseCardView,
@@ -110,14 +120,97 @@ class WriteInitFragment : Fragment() {
         super.onResume()
         Log.d("WriteInitFragment", "onResume")
 
-        bookVm.setBook(BookModel())
-        postVm.setPost(PostModel())
-        postVm.setImgUriList(listOf())
+        getUser()
+        getTotalPostCnt()
     }
 
     override fun onDetach() {
         super.onDetach()
         Log.d("WriteInitFragment", "onDetach")
+    }
+
+    private fun initAdapter() {
+        checklistContentAdapter.setMyChecklistContentRVAdapterListener(object :
+            ChecklistContentRVAdapter.MyChecklistContentRVAdapterListener {
+            override fun addChecklist(position: Int, content: String) {
+                createChecklist(position, content)
+            }
+
+            override fun updateChecklist(position: Int, toDoId: Long, content: String) {
+                val checklist =
+                    CheckListModel(toDoId = toDoId, content = content, isComplete = false)
+                modifyChecklist(position, checklist)
+            }
+
+            override fun completeChecklist(position: Int, checklist: CheckListModel) {
+                checklist.isComplete = true
+                modifyChecklist(position, checklist)
+            }
+
+        })
+
+        //체크리스트 리사이클러뷰 어댑터 설정
+        binding.checklistHiddenRv.setHasFixedSize(true)
+        binding.checklistHiddenRv.adapter = checklistContentAdapter
+    }
+
+    private fun createChecklist(position: Int, content: String) {
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val checklist = CheckListModel(content = content, isComplete = false)
+            checklistVm.createChecklist(checklist)
+//            checklistContentAdapter.notifyItemRangeChanged(position, 1)
+        }
+    }
+
+    private fun getUser() {
+        CoroutineScope(Dispatchers.Main).launch {
+            mainVm.getUser()
+        }
+    }
+
+    private fun modifyChecklist(position: Int, checklist: CheckListModel) {
+        CoroutineScope(Dispatchers.Main).launch {
+            checklistVm.updateChecklist(position, checklist)
+
+            if (checklist.isComplete) {
+                checklistContentAdapter.removeChecklist(position)
+            }
+        }
+    }
+
+    private fun setUI(user: UserModel) {
+        binding.writeInitTitleTv.text = "${user.nickname}님,\n오늘도 힘차게 기록해봅시다."
+
+        if (user.goal == "") {
+            binding.readingGoalTv.text = "독서 목표를 입력해주세요."
+        } else {
+            binding.readingGoalTv.text = user.goal
+        }
+    }
+
+    private fun setTotalPostCntUI(it: Int) {
+        binding.totalPagesTv.text = "total $it pages"
+    }
+
+    private fun observe() {
+        checklistVm.unCompletedChecklists.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            Log.d("WriteInitFragment", "checklists observe!! -> $it")
+
+            checklistContentAdapter.setChecklist(it)
+            checklistContentAdapter.notifyDataSetChanged()
+
+        })
+
+        mainVm.user.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            Log.d("WriteInitFragment", "user observe!! -> $it")
+            setUI(it)
+        })
+
+        postVm.totalCnt.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            Log.d("WriteInitFragment", "totalCnt observe!! -> $it")
+            setTotalPostCntUI(it)
+        })
     }
 
 }
