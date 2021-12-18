188ee6d35fca5423d3be3c25c9ff775e4dd74117
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-18 17:30:51 +0900

    Feat: 추가-체크리스트 추가, 수정

--- app/src/main/java/com/mangpo/bookclub/service/ChecklistService.kt
+++ app/src/main/java/com/mangpo/bookclub/service/ChecklistService.kt
@@ -1,14 +1,20 @@
 package com.mangpo.bookclub.service
 
+import com.mangpo.bookclub.model.CheckListModel
 import com.mangpo.bookclub.model.ChecklistsModel
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ChecklistService {
     @GET("/todos")
-    fun getChecklists(): Response<ChecklistsModel>
+    suspend fun getChecklists(): Response<ChecklistsModel>
 
-    @POST("/create-todos")
-    fun createChecklist(checklist: List<String>): Response<String>
+    @POST("/todos")
+    suspend fun createChecklist(@Body checklist: CheckListModel): Response<CheckListModel>
+
+    @PUT("/todos/{toDoId}")
+    suspend fun updateChecklist(
+        @Path("toDoId") toDoId: Long,
+        @Body checklist: CheckListModel
+    ): Response<String>
 }
