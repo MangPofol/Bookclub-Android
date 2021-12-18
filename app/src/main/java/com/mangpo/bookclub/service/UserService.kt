daa541ba15dfcd0687bc82dc436ca19c32dc3cdb
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-18 19:42:05 +0900

    Feat: 추가-getUser, createUser api URI 변경, updateUser 추가

--- app/src/main/java/com/mangpo/bookclub/service/UserService.kt
+++ app/src/main/java/com/mangpo/bookclub/service/UserService.kt
@@ -1,12 +1,15 @@
 package com.mangpo.bookclub.service
 
 import com.google.gson.JsonObject
+import com.mangpo.bookclub.model.UserDataModel
 import com.mangpo.bookclub.model.UserModel
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface UserService {
+
+    @GET("/users/current")
+    suspend fun getUser(): Response<UserDataModel>
 
     @POST("/auth/login")
     suspend fun login(@Body user: JsonObject): Response<JsonObject>
@@ -14,9 +17,12 @@ interface UserService {
     @POST("/users/validate-duplicate")
     suspend fun validateEmail(@Body email: JsonObject): Response<JsonObject>
 
-    @POST("/users")
+    @POST("/auth/signup")
     suspend fun createUser(@Body newUser: UserModel): Response<JsonObject>
 
     @POST("/logout")
     suspend fun logout(): Response<String>
+
+    @PUT("/users/{userId}")
+    suspend fun updateUser(@Path("userId") userId: Long, @Body user: UserModel): Response<String>
 }
