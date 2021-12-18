0aa479a8507f340353b0ec5cdb1a4e74738b1c2f
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-18 17:33:28 +0900

    Feat: 추가-ChecklistGroupByMonthModel, ChecklistModel

--- app/src/main/java/com/mangpo/bookclub/model/ChecklistsModel.kt
+++ app/src/main/java/com/mangpo/bookclub/model/ChecklistsModel.kt
@@ -1,10 +1,25 @@
 package com.mangpo.bookclub.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ChecklistsModel(
     var data: List<CheckListModel>
 )
 
+data class ChecklistGroupByMonthModel(
+    var groupingDate: String,
+    var checklists: ArrayList<CheckListModel>
+)
+
 data class CheckListModel(
-    var toDoId: Long,
-    var content: String
+    @SerializedName("toDoId")
+    var toDoId: Long? = null,
+    @SerializedName("content")
+    var content: String? = null,
+    @SerializedName("isComplete")
+    var isComplete: Boolean = false,
+    @SerializedName("createDate")
+    var createDate: String? = null,
+    @SerializedName("modifiedDate")
+    var modifiedDate: String? = null
 )
