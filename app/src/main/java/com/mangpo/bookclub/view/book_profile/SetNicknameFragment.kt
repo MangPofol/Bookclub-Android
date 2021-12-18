45b94a6708b8fd1ebfa49ff478770bc5684c7267
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-18 19:40:54 +0900

    Style: 코드 정리

--- app/src/main/java/com/mangpo/bookclub/view/book_profile/SetNicknameFragment.kt
+++ app/src/main/java/com/mangpo/bookclub/view/book_profile/SetNicknameFragment.kt
@@ -1,19 +1,13 @@
 package com.mangpo.bookclub.view.book_profile
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import com.mangpo.bookclub.databinding.FragmentSetNicknameBinding
-import com.mangpo.bookclub.model.UserModel
-import com.mangpo.bookclub.viewmodel.MainViewModel
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class SetNicknameFragment : Fragment(), TextWatcher {
 
